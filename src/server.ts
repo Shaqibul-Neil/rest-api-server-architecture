8b715bbed97af31ae5c972d275a8b1d460b10902
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-10 21:59:19 +0600

    Route Handling with HTTP Requests & Responses

--- src/server.ts
+++ src/server.ts
@@ -6,8 +6,14 @@ const server: Server = createServer((req: IncomingMessage, res) => {
   const method = req.method;
 
   if (url === "/" && method === "GET") {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Hello World");
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "Hello World" }));
+  } else if (url?.startsWith("/products")) {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "This is product route" }));
+  } else {
+    res.writeHead(404, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ message: "route not found" }));
   }
 });
 
