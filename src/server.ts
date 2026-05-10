adc23ca2fe07285d92bd571b91f2ba7fc14e9367
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-10 21:54:36 +0600

    creating server with node js

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import { createServer, IncomingMessage, Server } from "http";
+
+const server: Server = createServer((req: IncomingMessage, res) => {
+  //console.log(req.url, req.method);
+  const url = req.url;
+  const method = req.method;
+
+  if (url === "/" && method === "GET") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Hello World");
+  }
+});
+
+server.listen(5000, () => {
+  console.log("server running on port 5000");
+});
