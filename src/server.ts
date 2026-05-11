6f9fe7bfc7b5468963d5d211b60a54e2a802dbc9
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-11 14:43:53 +0600

    config files

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import { createServer, IncomingMessage, Server } from "http";
 import { routeHandler } from "./routes/routes";
+import config from "./config";
 
 const server: Server = createServer((req: IncomingMessage, res) => {
   routeHandler(req, res);
 });
 
-server.listen(3000, () => {
-  console.log("server running on port 3000");
+server.listen(config.port, () => {
+  console.log(`server running on port ${config.port}`);
 });
